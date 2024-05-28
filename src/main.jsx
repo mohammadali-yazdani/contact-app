6b67c42415ead9dab20ed33a2e1941e065b2a2e0
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2024-05-28 21:14:08 +0300

    Feat: Add MUI dep and customize direction to RTL & use custom font family

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+
+const theme = createTheme({
+  direction: "rtl",
+  typography: {
+    fontFamily: [
+      "Noto Sans Arabic",
+      "Roboto",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "-apple-system",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+});
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <CacheProvider value={cacheRtl}>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
+  </CacheProvider>
 );

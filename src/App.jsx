2371eba5ea4a377cd61491f4bc5adda91ef92cd0
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2024-06-02 13:08:01 +0300

    Feat: Register all components in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Container } from "@mui/material";
+import { AddForm } from "./components/AddForm";
+import { Header } from "./components/Header";
+import { ContactList } from "./components/ContactList";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [contacts, setContacts] = useState([]);
+
+  return (
+    <Container maxWidth="md">
+      <Header />
+      <AddForm setContacts={setContacts} />
+      <ContactList contacts={contacts} setContacts={setContacts} />
+    </Container>
+  );
 }
 
 export default App;

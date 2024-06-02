16b819ca10de8463b6d62783e1c53f78e4c8551d
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2024-06-02 13:06:55 +0300

    Refactor: Use css base line for reset css defaults

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import rtlPlugin from "stylis-plugin-rtl";
 import { prefixer } from "stylis";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
+import { CssBaseline } from "@mui/material";
 
 const theme = createTheme({
   direction: "rtl",
@@ -37,6 +38,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <CacheProvider value={cacheRtl}>
     <ThemeProvider theme={theme}>
       <React.StrictMode>
+        <CssBaseline />
         <App />
       </React.StrictMode>
     </ThemeProvider>
